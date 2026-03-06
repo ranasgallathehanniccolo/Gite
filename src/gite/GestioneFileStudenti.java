1798fda4c54213136a31f5923be17e99bb41eee2
ranasgallathehanniccolo <ranasgalla.thehanniccolo@franchettisalviani.net>
2026-03-06 12:17:18 +0100

    oroc

--- src/gite/GestioneFileStudenti.java
+++ src/gite/GestioneFileStudenti.java
@@ -12,5 +12,54 @@ import java.util.*;
  * @author ranasgalla.niccolo
  */
 public class GestioneFileStudenti {
+    private static final String FILE_STUDENTI = "studenti.dat";
+
     
+    public GestioneFileStudenti() {
+        creaSeNonEsiste(FILE_STUDENTI);
+        
+    }
+    private static final int DIM_RECORD = 100;
+    private void creaSeNonEsiste(String nomeFile) {
+        File f = new File(nomeFile);
+        if (!f.exists()) {
+            try {
+                f.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Impossibile creare " + nomeFile);
+            }
+        }
+    }
+    private String aggiustaLunghezzaStringa(String s) {
+        String aggiustata = s;
+        if (s.length() < 20) {
+            for (int i = 0; i < (20 - s.length()); i++) {
+                aggiustata += "*";
+            }
+            return aggiustata;
+        } else if (s.length() > 20) {
+            aggiustata = s.substring(0, 19);
+            return aggiustata;
+        }
+        return s;
+    }
+    public void AggiungiRecordStudente(Studente s){
+        try {
+            RandomAccessFile file = new RandomAccessFile("studenti.dat", "rw");
+            int nRecord = (int) (file.length() / DIM_RECORD);   
+            file.seek(nRecord * DIM_RECORD);
+            String nome = aggiustaLunghezzaStringa(s.getNome().trim());
+            String cognome = aggiustaLunghezzaStringa(s.getCognome().trim());
+            String classe = aggiustaLunghezzaStringa(s.getClasse().trim());
+            System.out.println(" " + nome);
+            file.writeChars(nome);
+            file.writeChars(cognome);
+            file.writeChars(classe);
+            file.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File non trovato");
+        } catch (IOException e) {
+            System.out.println("Problema in lettura-scrittura file");
+        }
+    }
 }
